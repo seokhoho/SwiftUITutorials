8a252e719148456365fc98ac70c59aaf873f0e75
Seokho Jeong <kreel9402@gmail.com>
2023-07-31 16:24:53 +0900

    feat Finish Combining Views

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,27 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by 정유진 on 2023/07/31.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("jeans")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 10)
+            }
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
