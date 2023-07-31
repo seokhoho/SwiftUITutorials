26c3a488c32e8826dbdd96e14a8b9460241ea351
Seokho Jeong <kreel9402@gmail.com>
2023-07-31 14:08:01 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 정유진 on 2023/07/31.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
